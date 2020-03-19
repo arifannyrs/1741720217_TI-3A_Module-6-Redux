e77e2f0a06df2f66aa49290306dfce1eb3ae55dd
arifannyrs <arifannyrs@gmail.com>
2020-03-19 12:15:08 +0700

    Buat file dengan nama MainReducer.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
 import MainReducer from './reducers/MainReducer'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = compose(window.devToolsExtension ? window.devToolsExtension() : f =>
-f)(createStore)(MainReducer)
+const store = compose(window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)(MainReducer) 
 
 ReactDOM.render(<Provider store={store}>
 <App />
